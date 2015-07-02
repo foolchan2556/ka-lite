89d1ebe5b975e3e4f263ad6d42145bbbc6917108
Aron Fyodor Asor <aron@learningequality.org>
2015-07-02 15:44:17 -0700

    don't use npm on vagrant provisioning.
    Extracting its dependencies requires a lot of memory, which our new
    vagrant vm doesn't have.

--- provision.sh
+++ provision.sh
@@ -4,9 +4,9 @@
 apt-get update --fix-missing
 apt-get install -y python2.7 python2.7-dev python-pip git phantomjs
 curl -sL https://deb.nodesource.com/setup_0.12 | bash -
-apt-get install -y nodejs
+# apt-get install -y nodejs
 pip install -r /home/vagrant/ka-lite/requirements_dev.txt
 cd /home/vagrant/ka-lite
 pip install -e .
 # --no-bin-links option is for linux guests on windows hosts
-npm install --no-bin-links
+# npm install --no-bin-links
