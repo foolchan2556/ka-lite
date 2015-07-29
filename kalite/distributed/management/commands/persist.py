3dd1a4b6e8898cf6556b2707956b2781ed9ea6ac
foolchan2556 <haolanchan@gmail.com>
2015-07-30 00:17:49 +0800

    further cleaned the PR

--- kalite/distributed/management/commands/persist.py
+++ kalite/distributed/management/commands/persist.py
@@ -1,28 +1,35 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
 import os
 import json
 from sqlitedict import SqliteDict
+
+logging = settings.LOG
+EXERCISES_FILEPATH = os.path.join(settings.CHANNEL_DATA_PATH, "exercises.json")
+EXERCISES_SQLITEPATH = os.path.join(
+    settings.CHANNEL_DATA_PATH, "exercises.sqlite")
+CONTENT_FILEPATH = os.path.join(settings.CHANNEL_DATA_PATH, "contents.json")
+CONTENT_SQLITEPATH = os.path.join(
+    settings.CHANNEL_DATA_PATH, "contents.sqlite")
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print("Converting...")
-        self.convert('data/khan/contents.json', 'data/khan/contents.sqlite')
-        self.convert('data/khan/exercises.json', 'data/khan/exercises.sqlite')
+        logging.info("Converting...")
+        self.convert(EXERCISES_FILEPATH, EXERCISES_SQLITEPATH)
+        self.convert(CONTENT_FILEPATH, CONTENT_SQLITEPATH)
 
     def convert(self, jsonurl, sqliteurl):
-        url = os.path.join(os.environ['KALITE_DIR'], jsonurl)
         try:
-            jsonfile = open(url)
+            jsonfile = open(jsonurl)
             items = json.load(jsonfile)
-            kalitedict = SqliteDict(
-                os.path.join(os.environ['KALITE_DIR'], sqliteurl))
-        except IOError, e:
-            print(e)
+            kalitedict = SqliteDict(sqliteurl)
+        except IOError as e:
+            logging.error(e)
             return
         for item in items:
             kalitedict[item] = items[item]
         kalitedict.commit()
         kalitedict.close()
-        print(jsonurl + " has been converted to " + sqliteurl)
+        logging.info(jsonurl + " has been converted to " + sqliteurl)
